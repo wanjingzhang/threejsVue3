0170d5d1fe6523a0781859941629031a16c31929
wanjingzhang <95616085@qq.com>
2022-07-13 07:31:43 +0800

    基本类型

--- basic/hello.js
+++ basic/hello.js
@@ -1,1 +1,7 @@
 console.log("Hello world!");
+// This is an industrial-grade general-purpose greeter function:
+function greet(person, date) {
+    console.log("Hello ".concat(person, ", today is ").concat(date, "!"));
+}
+greet("Brendan", new Date());
+var msg = "hello there!";
