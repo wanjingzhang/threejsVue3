06c2fe008de8edce4236420001cdb490578c0070
Celine <95616085@qq.com>
2022-07-13 15:45:15 +0800

    assets for mirror has been added.

---
+++ basic/2narrowing.ts
@@ -0,0 +1,19 @@
+// 1. in 操作符收窄 
+ 
+
+
+type Fish = { swim: () => void };
+type Bird = { fly: () => void };
+type Human = { swim?: () => void; fly?: () => void };
+ 
+function move(animal: Fish | Bird | Human) {
+  if ("swim" in animal) {
+    animal; // (parameter) animal: Fish | Human
+  } else {
+    animal; // (parameter) animal: Bird | Human
+  }
+}
+
+
+
+
