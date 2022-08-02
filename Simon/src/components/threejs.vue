6a9fcd29bc6952c639ef0ddb627ea9bc6fcc735f
wanjingzhang <95616085@qq.com>
2022-08-03 05:51:08 +0800

    使用三角函数移动你的camera，绕环一周查看物体

--- Simon/src/components/threejs.vue
+++ Simon/src/components/threejs.vue
@@ -132,8 +132,9 @@ onMounted(()=>{
       // cube.rotation.y += 0.01;
       // group.rotation.x += Math.sin(x++)
       // group.rotation.x += 0.01;
-      camera.position.x = cursor.x * 10
-      camera.position.y = cursor.y * 10
+      camera.position.x = Math.sin(cursor.x * Math.PI*2) * 3
+      camera.position.z = Math.cos(cursor.x * Math.PI*2) * 3
+      camera.position.y = cursor.y * 5 // 上下
       // camera.lookAt(new Vector3()) = 
       camera.lookAt(cube.position) 
 
